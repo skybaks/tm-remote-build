450e244bae837cd719711d962e693eaabef7c1d7
skybax <tmflame@gmail.com>
2023-08-05 09:03:59 -0500

    Add --openplanet argument to use default ports

--- src/tm_remote_build/cli.py
+++ src/tm_remote_build/cli.py
@@ -13,8 +13,15 @@ DEFAULT_PORTS = {
 }
 
 
+def get_port(args) -> int:
+    if args.port is not None:
+        return args.port
+    else:
+        return DEFAULT_PORTS.get(args.openplanet, 0)
+
+
 def unload(args) -> None:
-    api = RemoteBuildAPI(args.port)
+    api = RemoteBuildAPI(get_port(args))
     unloaded = api.unload_plugin(args.plugin_id)
     if unloaded:
         logger.info('Commanded unload for plugin with ID "%s"' % (args.plugin_id,))
@@ -25,7 +32,7 @@ def unload(args) -> None:
 
 
 def load(args) -> None:
-    api = RemoteBuildAPI(args.port)
+    api = RemoteBuildAPI(get_port(args))
     loaded = api.load_plugin(
         args.plugin_id, plugin_src=args.plugin_src, plugin_type=args.plugin_type
     )
@@ -38,33 +45,42 @@ def load(args) -> None:
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser(prog="tm-remote-build")
+    parser = argparse.ArgumentParser(
+        prog="tm-remote-build", description="Load or unload Openplanet plugins"
+    )
     subparser = parser.add_subparsers()
 
-    def common_args(subparser) -> None:
-        subparser.add_argument(
+    def common_args(sub_input) -> None:
+        sub_input.add_argument(
             "plugin_id",
             help="The plugin ID to be targeted. For a folder source plugin this would be the folder name. For a zipped source plugin this would be the filename without extension.",
         )
-        subparser.add_argument(
+        sub_input.add_argument(
             "-p",
             "--port",
-            default=30000,
             type=int,
             help="The port used to communicate with Openplanet",
         )
-        subparser.add_argument(
+        sub_input.add_argument(
+            "-op",
+            "--openplanet",
+            choices=["OpenplanetNext", "Openplanet4", "OpenplanetTurbo"],
+            help="Alternative to entering port number. Will use the default port for that game.",
+        )
+        sub_input.add_argument(
             "-v",
             "--verbose",
             action="store_true",
             help="Enable verbose logging for debugging Remote Build to Openplanet communication",
         )
+        sub_input.description = "Specify at least one of [--port, --openplanet] to enable communication with the Remote Build plugin running in the game. If both are specified, --port will take precedence."
 
     sub_unload = subparser.add_parser("unload", help="Unload a plugin")
-    common_args(sub_unload)
     sub_unload.set_defaults(func=unload)
+    common_args(sub_unload)
 
     sub_load = subparser.add_parser("load", help="Load a plugin")
+    sub_load.set_defaults(func=load)
     sub_load.add_argument(
         "plugin_type",
         choices=["folder", "zip"],
@@ -77,8 +93,11 @@ def main() -> None:
         default="user",
         help='The source location to load plugin from where "user" is the OpenplanetX/Plugins folder in C:\\Users and "app" is the Openplanet/Plugins folder in the game directory. Default is "user" if unspecified.',
     )
-    sub_load.set_defaults(func=load)
 
     args = parser.parse_args()
+
+    if args.openplanet is None and args.port is None:
+        parser.error("Must specify at least one of [--port, --openplanet]")
+
     logging.basicConfig(level=logging.DEBUG if args.verbose else logging.ERROR)
     args.func(args)
